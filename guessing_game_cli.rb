77e33bed0a1d2b64aaa3ca7b7c2e5f825791ae22
Scot Sexton <rss3509@gmail.com>
2020-07-08 23:18:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   rando = rand(6)+1 
   puts "Guess a number between 1 and 6:"
   input = gets.chomp
-  if rando == input.to_s
+  if input == rando.to_s
     puts "You guessed the correct number!"
   elsif input.downcase == "exit"
   puts "Goodbye!"
